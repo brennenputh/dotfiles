e60e9d93e3ffb3a328aba4095ff947f7f37c3a4a
brennenputh <brennenputh@gmail.com>
2023-10-18 08:36:57 -0400

    Add snippet fix

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -53,6 +53,19 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
   end,
 })
 
+-- Makes it so snippets don't mess up tab
+vim.api.nvim_create_autocmd('ModeChanged', {
+  pattern = '*',
+  callback = function()
+    if ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+        and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not require('luasnip').session.jump_active
+    then
+      require('luasnip').unlink_current()
+    end
+  end
+})
+
 vim.api.nvim_create_autocmd("QuitPre", {
   callback = function()
     local invalid_win = {}

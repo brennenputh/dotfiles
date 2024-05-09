c76d7e4447f6106389c808c05dcd871771427c34
Brennen Puth <brennenputh@gmail.com>
2024-05-09 16:53:06 -0400

    AHA

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,10 @@ end
 set -g fish_greeting ''
 
 oh-my-posh init fish --config "$HOME/.config/fish/themes/catppuccin_macchiato.omp.json" | source
+
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end

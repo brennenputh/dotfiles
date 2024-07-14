c6af1bd78be42b3ae8e49d045568b02c73764bde
Brennen Puth <brennenputh@gmail.com>
2024-07-14 18:42:26 -0400

    Just tell treesitter to install everything

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 local M = {
 	"nvim-treesitter/nvim-treesitter",
 	build = function()
-		require("nvim-treesitter.install").update({ with_sync = true })()
+		require("nvim-treesitter.install").update({ with_sync = false })()
 	end,
 }
 
@@ -9,11 +9,7 @@ function M.config()
 	local configs = require("nvim-treesitter.configs")
 
 	configs.setup({
-    ensure_installed = {
-      "markdown_inline"
-    },
-		sync_install = false,
-    auto_install = true,
+    ensure_installed = "all",
 		highlight = { enable = true },
 		indent = { enable = true },
 		autopairs = { enable = true },

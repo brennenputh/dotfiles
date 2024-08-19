4d46d9dc439f25025440fdbf5f7d47fc1e6df3b1
brennenputh <brennenputh@gmail.com>
2024-08-19 15:15:20 -0400

    AAAA

--- fish/config.fish
+++ fish/config.fish
@@ -6,10 +6,6 @@ set -g fish_greeting ''
 
 oh-my-posh init fish --config "$HOME/.config/fish/themes/catppuccin_macchiato.omp.json" | source
 
-if $HOME/bin/detect_wsl
-  set -g WINDRIVE '/mnt/c/'
-end
-
 # Start X at login
 if status is-login
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
@@ -23,3 +19,5 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+
